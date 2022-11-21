daf08e13d2427ccd0aba0e88351567801b1fea11
Ondrej Mottl <ondrej.mottl@gmail.com>
2022-11-21 12:40:00 +0100

    change `silent` to `verbose`

--- R/check_if_loaded.R
+++ R/check_if_loaded.R
@@ -1,19 +1,19 @@
 #' @title Confirm that the selected file is present in selected environment
 #' @param file_name Name of the selected file
 #' @param env The environment to test the presence of the file in
-#' @param silent Logical. Should there be no message?
+#' @param verbose Logical. Should there be output message?
 #' @export
 check_if_loaded <-
   function(file_name,
            env = rlang::current_env,
-           silent = FALSE) {
+           verbose = TRUE) {
     check_class("file_name", "character")
 
     check_class("env", "environment")
 
 
     if (
-      silent == FALSE
+      verbose == TRUE
     ) {
       stop_if_not(
         exists(
