3c2d528cf2137c0c213d7570e4b6010f986d29bb
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-01-19 16:42:36 +0100

    update argument names in `do_in_time()`

--- R/do_in_time.R
+++ R/do_in_time.R
@@ -2,23 +2,24 @@
 #' @description A wrapper to `R.utils::withTimeout()`
 #' @param expr The R expression to be evaluated
 #' @param envir The environment in which the expression should be evaluated
-#' @param timeout A numeric specifying the maximum number of seconds the 
-#' expression is allowed to run before being interrupted by the timeout
-#' @param onTimeout A character specifying what action to take if 
+#' @param timeout A numeric specifying the maximum number of seconds the
+#' expression is allowed to run before being interrupted by the timeout see
+#' [R.utils::withTimeout()] for more information. Defaul is `"silent"`
+#' @param on_timeout A character specifying what action to take if
 #' a `timeout`` event occurs
 #' @param ... other parameters passed to `R.utils::withTimeout()`
 #' @seealso [R.utils::withTimeout()]
-#' @export 
+#' @export
 do_in_time <- function(expr,
                        envir = parent.frame(),
                        timeout,
-                       onTimeout = c("error", "warning", "silent"),
+                       on_timeout = "silent",
                        ...) {
   R.utils::withTimeout(
     expr = expr,
     envir = envir,
     timeout = timeout,
-    onTimeout = onTimeout,
+    onTimeout = on_timeout,
     ...
   )
 }
