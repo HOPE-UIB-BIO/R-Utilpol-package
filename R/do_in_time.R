cebaf2fcfd7297c01c362005d123b20bd2d0db6f
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-01-19 18:17:31 +0100

    rework `do_in_time` without `R.utils::withTimeout`

--- R/do_in_time.R
+++ R/do_in_time.R
@@ -1,25 +1,22 @@
 #' @title Evaluate an expression and interrupts it if it takes too long
-#' @description A wrapper to `R.utils::withTimeout()`
 #' @param expr The R expression to be evaluated
 #' @param envir The environment in which the expression should be evaluated
 #' @param timeout A numeric specifying the maximum number of seconds the
-#' expression is allowed to run before being interrupted by the timeout see
-#' [R.utils::withTimeout()] for more information. Defaul is `"silent"`
-#' @param on_timeout A character specifying what action to take if
-#' a `timeout`` event occurs
-#' @param ... other parameters passed to `R.utils::withTimeout()`
-#' @seealso [R.utils::withTimeout()]
+#' expression is allowed to run before being interrupted by the `timeout`
+#' @details Function heavily instipred by [R.utils::withTimeout()]
 #' @export
 do_in_time <- function(expr,
                        envir = parent.frame(),
-                       timeout,
-                       on_timeout = "silent",
-                       ...) {
-  R.utils::withTimeout(
-    expr = expr,
-    envir = envir,
-    timeout = timeout,
-    onTimeout = on_timeout,
-    ...
+                       timeout) {
+  expr <- substitute(expr)
+  setTimeLimit(cpu = timeout, elapsed = timeout, transient = TRUE)
+  on.exit({
+    setTimeLimit(cpu = Inf, elapsed = Inf, transient = FALSE)
+  })
+  try(
+    {
+      eval(expr, envir = envir)
+    },
+    silent = TRUE
   )
 }
